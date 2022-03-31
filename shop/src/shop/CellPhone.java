b60ae73c7f97572375b4faca219489e2024b8890
dongryulmin <hackingnono11@naver.com>
2022-03-31 18:45:06 +0900

    aSd

--- shop/src/shop/CellPhone.java
+++ shop/src/shop/CellPhone.java
@@ -5,10 +5,6 @@ public class CellPhone extends Product {
 	private String carrier; // 통신사 정보
 	
 	
-<<<<<<< HEAD
-	
-=======
->>>>>>> branch 'master' of https://github.com/MinDongRyul/javasource.git
 	public CellPhone(String name, int price, String carrier) {
 		super(name, price);
 		this.carrier = carrier;
