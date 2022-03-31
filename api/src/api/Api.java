37ff31e50ace0ce6e0d8d44aaa988219d930bbff
dongryulmin <hackingnono11@naver.com>
2022-03-31 18:54:00 +0900

    apiupdate

--- api/src/api/Api.java
+++ api/src/api/Api.java
@@ -6,13 +6,8 @@ public class Api {
 	/*  API(Application Programming Interface) == 라이브러리
 	 *  프로그램 개발에 자주 사용되는 클래스 및 인터페이스 모음
 	 *  
-<<<<<<< HEAD
-	 *  자바 APIㅁ
-=======
-	 *  자바 API
->>>>>>> branch 'master' of https://github.com/MinDongRyul/javasource.git
-	 * 
-	 */
+	*/
+
 	
 	String str = "";
 	Scanner sc = new Scanner(System.in);
